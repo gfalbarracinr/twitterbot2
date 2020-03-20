546abd93196156fcd79dda40dcc597bb03479aa5
Giovanny Albarracin <albarracingiovanny@gmail.com>
2020-03-19 23:16:08 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const Twit = require('twit');
+const config = require('./config.js');
+const axios = require('axios');
+var schedule = require('node-schedule');
+
+
+const T = new Twit(config);
+
+T.get('account/verify_credentials',  {
+    include_entities: false,
+    skip_status: true,
+    include_email: false
+}, onAuthenticated);
+
+
+const getData = async () =>  {
+    const response = await axios.get('https://corona.lmao.ninja/countries/colombia');
+    const data = await response.data;
+    return data;
+        
+}
+
+async function onAuthenticated(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Authentication succesful.');
+    }   
+
+    changeStatus();
+     
+}
+
+async function changeStatus() {
+    const response = await getData();
+    console.log(response);
+    T.post(
+        'statuses/update',
+        {
+            status: `Reporte de casos de Coronavirus Colombia
+                Infectados: ${response.cases}
+                Recuperados: ${response.recovered}
+                Muertos: ${response.deaths}
+            `
+        }, onTweeted
+    );
+}
+
+function onTweeted(err) {
+    if (err) {
+        console.log('Twitter error: ', err.message);
+    } else {
+        console.log('A new report was generated.');
+    }
+}
+
+var j = schedule.scheduleJob('*/5 * * * *', function(fireDate){
+     console.log('Scheduler running at ', fireDate);
+     changeStatus();
+});
