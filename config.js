27f22b2eef88a367f52cfad979d8e63fac28845a
gfalbarracinr <gfalbarracinr@unal.edu.co>
2020-03-19 23:43:54 -0500

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: 'X4arcygn5ZS87O3uy3U5duhyA' ,
-    consumer_secret: 'IbTj2gQ0qzofkxELRVhAXiWGhmKVz1b3hptAo3R6o3LWB1pQlb',
-    access_token: '1240014985939636224-gwYm7PK1FYu8Ef0T4mtBLSMXjsRE3D',
-    access_token_secret: 'rfqlYBdZP7b6emslnlX8XogfpYdRMdPCZT7Eqqqowb9pL'
-}
